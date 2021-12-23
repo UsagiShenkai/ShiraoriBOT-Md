b7b3e6f9518041a526ffe321f49098ad98861b4e
UsagiShenkai <91740250+UsagiShenkai@users.noreply.github.com>
2021-12-23 22:49:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -10,15 +10,15 @@ let duit = `*────── 「 DONATE 」 ──────*
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
-├🏧 081351047727 (OVO/Dana/GoPay)
-├📍 https://saweria.co/ilmanhdyt
+├🏧 085717026352 (OVO/Dana/GoPay)
+├📍 https://saweria.co/aldighdx
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 
 Arigatou!
 
 Contact person Owner:
-wa.me/6181351047727 (Owner)`
+wa.me/6285717026352 (Owner)`
 let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/ca517a39ce85c8a4c0479.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
@@ -29,14 +29,14 @@ let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/fil
            hydratedButtons: [{
              urlButton: {
                displayText: '🏧 Saweria',
-               url: 'https://saweria.co/ilmanhdyt'
+               url: 'https://saweria.co/aldighdx'
              }
 
            },
                {
              callButton: {
                displayText: 'Telkomsel',
-               phoneNumber: '+62 813-5104-7727'
+               phoneNumber: '+62 813-8476-7392'
              }
            },           
                {
